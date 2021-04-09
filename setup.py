8f6957437f4c951fceec519a2b4eef3215b2875d
jmiller <jmiller@proofpoint.com>
2021-04-09 12:33:46 -0700

    Update install requires / requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'conflux-python',         # How you named your package folder (MyLib)
   packages = ['conflux-python'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'python-based implementation of Conflux protocol',   # Give a short description about your library
   author = 'Justin Miller',                   # Type in your name
@@ -10,11 +10,7 @@ setup(
   url = 'https://github.com/jmillpps/conflux-python',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/jmillpps/conflux-python/v_02.tar.gz',    # I explain this later on
   keywords = ['Conflux'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'hashlib',
-          'ecdsa',
-          'Crypto.Hash',
-      ],
+  install_requires = [],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
